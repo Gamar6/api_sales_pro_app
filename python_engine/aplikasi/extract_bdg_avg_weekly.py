51bdd6bfdc016e30e6f40e02ca650d648f51556c
Mohamad Gamar <gamarbaik69@gmail.com>
2026-09-03 09:47:06 +0700

    Remove sensitive ERP credentials
    Removed sensitive ERP configuration details from the script.

--- python_engine/aplikasi/extract_bdg_avg_weekly.py
+++ python_engine/aplikasi/extract_bdg_avg_weekly.py
@@ -10,11 +10,6 @@ import numpy as np
 # 1. KONFIGURASI ERP
 # ==========================================================
 
-url = "http://erp.fivafood.co.id:8069"
-db = "fivaDB"
-username = "denny.monoarfa@fivafood.co.id"
-password = "Pinky1677"
-
 # ==========================================================
 # 2. CONNECT ERP
 # ==========================================================
